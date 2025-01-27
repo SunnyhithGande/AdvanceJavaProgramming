8269c6735f42a55524c53146ec9b2a7ec8c9462a
Sunnyhith Gande <165890829+SunnyhithGande@users.noreply.github.com>
2025-01-27 11:38:50 +0530

    Update BasicGraphicsExample.java

--- BasicGraphicsExample.java
+++ BasicGraphicsExample.java
@@ -5,11 +5,14 @@ public class BasicGraphicsExample extends JComponent
 {
 	public void paint(Graphics g)
 	{
+		Color c = new Color(255, 255, 255);	
+		g.setColor(c);
 		g.drawRect(100,50,50,200);
+		g.fillRect(100,50,50,200);
 	}
 	public static void main(String args[])
 	{
-		JFrame frame = new JFrame("Basic frame");
+		JFrame frame = new JFrame("Basic Graphics");
 		frame.setSize(500,500);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
